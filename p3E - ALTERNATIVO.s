361aaf6f1782bc74e239d824b526178a43560121
Sergio Castell <sergiocastell@users.noreply.github.com>
2016-08-31 18:50:04 +0200

    Limpieza del código

--- p3E - ALTERNATIVO.s
+++ p3E - ALTERNATIVO.s
@@ -2,15 +2,15 @@
 
 __start:
 		li $t0,44				#44=Coma
-		sb $t0,0x10020001		#Coma->0x10020001, justo al lado de 0x10020000, que es el numero a mostrar
+		sb $t0,0x10020001			#Coma->0x10020001, justo al lado de 0x10020000, que es el numero a mostrar
 		li $t0,91				#91=[, el caracter despues de la Z
-		sb $t0,0x10020000		#91->0x10020000
+		sb $t0,0x10020000			#91->0x10020000
 		
 bucle:
-		sub $t0,$t0,1			#Resta de una ud. a t0
-		sb 	$t0,0x10020000		#t0->0x10020000
-		la 	$a0,0x10020000		#Cargamos 0x10020000 en a0 para mostrarlo por pantalla #la $a0,0x10020000+0x10020001
-		beq $t0,65,ultimopaso	#Si t0=65, saltamos a ultimopaso
+		sub $t0,$t0,1				#Resta de una ud. a t0
+		sb 	$t0,0x10020000			#t0->0x10020000
+		la 	$a0,0x10020000			#Cargamos 0x10020000 en a0 para mostrarlo por pantalla #la $a0,0x10020000+0x10020001
+		beq $t0,65,ultimopaso			#Si t0=65, saltamos a ultimopaso
 		jal put_str				#JAL, saltamos a put_str (escribir_string)
 		j	bucle				#Volvemos a empezar el bucle
 		
@@ -21,7 +21,7 @@ put_str:
 		
 ultimopaso:						#ultimo paso: eliminamos la coma final y mostramos a por pantalla
 		li 	$t1,0				#cargamos 0 en t1
-		sb 	$t1,0x10020001		#al cargar 0 en 0x10020001, eliminamos la coma que teniamos anteriormente guardada (44)
+		sb 	$t1,0x10020001			#al cargar 0 en 0x10020001, eliminamos la coma que teniamos anteriormente guardada (44)
 		jal put_str				#JAL, saltamos a put_str (escribir_string)
 		j fin					#Jump a fin
 fin:
