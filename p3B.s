cc99c0296bd030e6b2f1c806e8ca7f4af99ad302
Sergio Castell <sergiocastell@users.noreply.github.com>
2016-08-31 18:49:32 +0200

    Limpieza del código

--- p3B.s
+++ p3B.s
@@ -9,13 +9,13 @@ __start:
 	la	$a0,txt_1		# Mostramos txt_1 por pantalla
 	li	$v0,4			# Preparamos salida por pantalla de cadena
 	syscall				# llamada al sistema
-	li	$a0,0x10010100	# cargamos 0x10010100 en a0 para que el codigo se guarde a partir de esa direccion
+	li	$a0,0x10010100		# cargamos 0x10010100 en a0 para que el codigo se guarde a partir de esa direccion
 	li	$a1,31			# cargamos 31 en a1, ya que si ponemos 30 solo dejaria al usuario introducir 29 caracteres
 	jal get_cad			# JAL, saltamos a get_cad para obtener una cadena de texto
 	la	$a0,txt_2		# Mostramos txt_2 por pantalla
 	li	$v0,4			# Preparamos salida por pantalla de cadena
 	syscall				# llamada al sistema
-	li	$a0,0x10010200	# cargamos 0x10010200 en a0 para que el codigo se guarde a partir de esa direccion
+	li	$a0,0x10010200		# cargamos 0x10010200 en a0 para que el codigo se guarde a partir de esa direccion
 	li	$a1,41			# cargamos 41 en a1, ya que si ponemos 40 solo dejaria al usuario introducir 39 caracteres
 	jal	get_cad			# JAL, saltamos a get_cad para obtener una cadena de texto
 	li	$v0,10			# Cierra consola y finaliza programa.
