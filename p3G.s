7c4abe202d579c28f33157b7ff39a1dd2e0cd24b
Sergio Castell <sergiocastell@users.noreply.github.com>
2016-08-31 18:46:58 +0200

    Para imprimir la cadena no es necesario usar put_str*
    La cadena es cargada en la propia sección de L*, por lo que no es necesario hacer uso de secuencias de put_str más que una genérica. Así, la tabla de saltos se mantiene ya que en todos los conjuntos de instrucciones hay la misma cantidad de líneas
    
    Signed-off-by: Sergio Castell <sergiocastell@github.com>

--- p3G.s
+++ p3G.s
@@ -16,40 +16,40 @@ __start:
 	li	$t2,20				#20 ya que en este caso se da cada 14 en hex. (20 en dec.) cuando hay una nueva etiqueta L0,L1,L2...
 	la	$a0,str_0			#Introduce k	
 	jal put_str				#JAL, salta a Escribir Cadena
-	jal read_int			#JAL, salta a Leer Entero
+	jal read_int				#JAL, salta a Leer Entero
 	move $t4,$2				#Mueve de $2 a t4 para continuar la ejecucion
 	
 	slt	$s0,$t4,$0			#compara k con limite inferior 0
-	bne	$s0,$0,finalt		#Si k < 0 salta a fin
+	bne	$s0,$0,finalt			#Si k < 0 salta a fin
 	sgt	$s0,$t4,$t1			#compara k con limite superior+1
-	bne $s0,$0,finalt		#si k>4, salta a fin
+	bne $s0,$0,finalt			#si k>4, salta a fin
 	
-	mul $t5,$t4,$t2			#multiplicamos k por 20, ya que las direcciones de la tabla de salto se dan cada 20 (14 en hex.)
+	mul $t5,$t4,$t2				#multiplicamos k por 20, ya que las direcciones de la tabla de salto se dan cada 20 (14 en hex.)
 	la $t3,L0				#cargamos la direccion de memoria de L0 para usarla de base
-	add $t3,$t3,$t5			#sumamos el valor de la multiplicacion de k por 20 a la dir. base. 
-	jr $t3					#saltamos a la direccion deseada
+	add $t3,$t3,$t5				#sumamos el valor de la multiplicacion de k por 20 a la dir. base. 
+	jr $t3					#saltamos a la direccion deseada de la tabla
 	
 L0:	la	$a0,str_1			#L0: muestra por pantalla la eleccion de caso y hace la multiplicacion de L6 y L7
 	jal put_str				#JAL, salta a Escribir Cadena
 	mul	$t8,$t6,$t7			#Multiplica t6 y t7 y guarda el resultado en t8
-	j	Fin					#J, salta a Fin
+	j	Fin				#J, salta a Fin
 L1:	la	$a0,str_2			#L1: muestra por pantalla la eleccion de caso y hace la resta de L6 y L7
 	jal put_str				#JAL, salta a Escribir Cadena
 	sub	$t8,$t6,$t7			#Resta t6 y t7 y lo guarda en t8
-	j	Fin					#J, salta a Fin
+	j	Fin				#J, salta a Fin
 L2:	la	$a0,str_3			#L2: muestra por pantalla la eleccion de caso y hace el OR de L6 y L7
 	jal put_str				#JAL, salta a Escribir Cadena
 	or	$t8,$t6,$t7			#Hace el or logico de t6 y t7 y lo guarda en t8
-	j	Fin					#J, salta a Fin
+	j	Fin				#J, salta a Fin
 L3:	la	$a0,str_4			#L3: muestra por pantalla la eleccion de caso y hace el and de L6 y L7
 	jal put_str				#JAL, salta a Escribir Cadena
 	and	$t8,$t6,$t7			#Hace el and logico de t6 y t7 y lo guarda en t8
-	j	Fin					#J, salta a Fin
+	j	Fin				#J, salta a Fin
 
 Fin:						#Final: Muestra el valor de C y finaliza el programa
 	la	$a0,str_5			#carga str_5 para mostrarlo por pantalla
 	jal put_str				#JAL, salta a Escribir Cadena
-	move $a0,$t8			#Mueve el valor de las operaciones de t8 a a0 para escribirlo en pantalla
+	move $a0,$t8				#Mueve el valor de las operaciones de t8 a a0 para escribirlo en pantalla
 	jal put_int				#JAL, salta a Escribir entero
 	la	$a0,enter			#Escribimos 2 saltos de linea para evitar que se junten al reiniciar el programa
 	jal put_str				#JAL, salta a Escribir Cadena
@@ -72,32 +72,6 @@ put_str:
 	la	$v0,4				#escribe cadena de texto
 	syscall					#llamada al sistema
 	jr $31					#vuelve a la rutina principal
-	
-put_str1:
-	
-	la	$v0,4				#escribe cadena de texto
-	syscall					#llamada al sistema
-	jr $31					#vuelve a la rutina principal
-	
-put_str2:
-	la	$a0,str_2
-	la	$v0,4				#escribe cadena de texto
-	syscall					#llamada al sistema
-	jr $31					#vuelve a la rutina principal
-	
-put_str3:
-	la	$a0,str_3
-	la	$v0,4				#escribe cadena de texto
-	syscall					#llamada al sistema
-	jr $31					#vuelve a la rutina principal
-	
-put_str4:
-	la	$a0,str_4
-	la	$v0,4				#escribe cadena de texto
-	syscall					#llamada al sistema
-	jr $31					#vuelve a la rutina principal
-	
-	
 	
 read_int:
 	la	$v0,5				#lee entero de consola
